33fc447b6dabbe52d096cd634277d9f61a9b8353
Julia Benito <julia.benito@babelgroup.com>
2026-03-24 10:31:30 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'yourDictionary',
+  webDir: 'www'
+};
+
+export default config;
